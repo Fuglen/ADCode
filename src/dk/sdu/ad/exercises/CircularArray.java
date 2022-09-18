1344f4059a952a1fbdedd5587a98436494985290
Simon Fugl <sifug20@student.sdu.dk>
2022-09-18 21:31:46 +0200

    Add CircularArray class

---
+++ src/dk/sdu/ad/exercises/CircularArray.java
@@ -0,0 +1,20 @@
+package dk.sdu.ad.exercises;
+
+public class CircularArray {
+
+    public int[] circularArr = new int[10];
+
+    CircularArray() {
+    }
+
+    public void dequeue() {
+
+    }
+    public void enqueue(int n) {
+        for (int i = circularArr.length-1; i < 0; i--) {
+            if(circularArr.equals(null)) {
+                circularArr.
+            }
+        }
+    }
+}
